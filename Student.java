60a100a150aaaa4a6e6112489fc21197f7182e26
Ishan <punjishanrenu@gmail.com>
2025-05-27 11:30:52 +0530

    added new method main

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-class Student{
+public class Student{
     public int rollno;
     public String name;
     public int age;
@@ -14,5 +14,21 @@ class Student{
         if (Average()<60){
             return "B";
         }
+    }
+}
+class StudentTest{
+    
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.city="Sundernagar";
+        s.m1=80;
+        s.m2=90;
+        s.m3=89;
+        System.out.println("The student lives in "+ s.city);
+        System.out.println("Average is "+s.Average());
+        System.out.println("Grade is"+s.grade());
+
+    
+
     }
 }
